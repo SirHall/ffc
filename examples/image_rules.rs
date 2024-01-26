7f90dc2f8ae6de2a3b39f2ee1873c715068e7720
SirHall <28499210+SirHall@users.noreply.github.com>
2024-01-26 14:45:23 +1000

    Fix collapse_rule bugs and expand collapse rules

--- examples/image_rules.rs
+++ examples/image_rules.rs
@@ -3,7 +3,7 @@ use ffc::{
     ffc::collapse_rules::{collapse_rule, CollapseRule},
     prelude::*,
 };
-use image::{DynamicImage, GenericImage, Pixel, Rgb, Rgba};
+use image::{DynamicImage, GenericImage, Pixel, Rgb};
 use std::path::PathBuf;
 
 /// Example application of FFC, allowing the generation of collapsed images of far greater size than before
@@ -18,8 +18,10 @@ struct Args {
     height: Option<usize>,
     #[clap(short, long)]
     radius: Option<usize>,
-    #[clap(long)]
-    wrap: bool,
+    // #[clap(long)]
+    // wrap: bool,
+    #[clap(short, long)]
+    seeds: Option<usize>,
 
     #[clap(long)]
     reroll_attempts: Option<usize>,
@@ -38,7 +40,9 @@ struct Args {
 enum Tile {
     Unset,
     Outer,
+    DeepWater,
     Water,
+    ShallowWater,
     Sand,
     Grass,
     Forest,
@@ -53,6 +57,8 @@ fn main() {
     let unset = Tile::Unset;
     let outer = if unset_and_outer_are_equal { unset } else { Tile::Outer };
 
+    let seeds = args.seeds.unwrap_or(1);
+
     let width = args.width.unwrap_or(16);
     let height = args.height.unwrap_or(16);
     let radius = args.radius.unwrap_or(1);
@@ -60,39 +66,88 @@ fn main() {
     let grid = initialize(width, height, unset);
     let history_grid = initialize(width, height, unset);
 
-    let tile_options = vec![Tile::Water, Tile::Sand, Tile::Grass, Tile::Forest, Tile::Mountain];
+    let tile_options = vec![
+        Tile::Water,
+        Tile::ShallowWater,
+        Tile::Sand,
+        Tile::Grass,
+        Tile::Forest,
+        Tile::Mountain,
+    ];
 
     let unset_tile_rule = CollapseRule::False;
     let outer_tile_rule = CollapseRule::False;
-    let water_tile_rule = CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
-        CollapseRule::Not(Box::new(CollapseRule::InBounds)),
+    let deep_water_tile_rule = CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
+        CollapseRule::Is(Tile::Outer),
+        CollapseRule::Is(Tile::DeepWater),
+        CollapseRule::Is(Tile::Water),
+        // CollapseRule::Is(Tile::Mountain),
+    ])));
+    let water_tile_rule = CollapseRule::And(vec![
+        CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
+            CollapseRule::Is(Tile::Water),
+            CollapseRule::Is(Tile::Outer),
+            CollapseRule::Is(Tile::DeepWater),
+            CollapseRule::Is(Tile::ShallowWater),
+            // CollapseRule::Is(Tile::Forest),
+        ]))),
+        CollapseRule::Not(Box::new(CollapseRule::Near(
+            Box::new(CollapseRule::Or(vec![
+                CollapseRule::Is(Tile::Sand),
+                CollapseRule::Is(Tile::Grass),
+            ])),
+            3,
+        ))),
+    ]);
+    let shallow_water_tile_rule = CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
+        CollapseRule::Is(Tile::ShallowWater),
         CollapseRule::Is(Tile::Water),
         CollapseRule::Is(Tile::Sand),
     ])));
-    let sand_tile_rule = CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
-        CollapseRule::Is(Tile::Sand),
-        CollapseRule::Is(Tile::Water),
-        CollapseRule::Is(Tile::Grass),
-    ])));
-    let grass_tile_rule = CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
-        CollapseRule::Is(Tile::Grass),
-        CollapseRule::Is(Tile::Sand),
-        CollapseRule::Is(Tile::Forest),
-    ])));
-    let forest_tile_rule = CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
-        CollapseRule::Is(Tile::Forest),
-        CollapseRule::Is(Tile::Grass),
-        CollapseRule::Is(Tile::Mountain),
-    ])));
-    let mountain_tile_rule = CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
-        CollapseRule::Is(Tile::Mountain),
-        CollapseRule::Is(Tile::Forest),
-    ])));
+    let sand_tile_rule = CollapseRule::And(vec![
+        CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
+            CollapseRule::Is(Tile::Sand),
+            CollapseRule::Is(Tile::ShallowWater),
+            CollapseRule::Is(Tile::Grass),
+            // CollapseRule::Is(Tile::Mountain),
+        ]))),
+        CollapseRule::Not(Box::new(CollapseRule::Near(Box::new(CollapseRule::Is(Tile::Outer)), 5))),
+    ]);
+    let grass_tile_rule = CollapseRule::And(vec![
+        CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
+            CollapseRule::Is(Tile::Grass),
+            CollapseRule::Is(Tile::Sand),
+            CollapseRule::Is(Tile::Forest),
+        ]))),
+        CollapseRule::Not(Box::new(CollapseRule::Near(Box::new(CollapseRule::Is(Tile::Outer)), 6))),
+    ]);
+
+    let forest_tile_rule = CollapseRule::And(vec![
+        CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
+            CollapseRule::Is(Tile::Forest),
+            CollapseRule::Is(Tile::Grass),
+            CollapseRule::Is(Tile::Mountain),
+            // CollapseRule::Is(Tile::Water),
+        ]))),
+        CollapseRule::Not(Box::new(CollapseRule::Near(Box::new(CollapseRule::Is(Tile::Outer)), 7))),
+    ]);
+
+    let mountain_tile_rule = CollapseRule::And(vec![
+        CollapseRule::NextTo(Box::new(CollapseRule::Or(vec![
+            CollapseRule::Is(Tile::Mountain),
+            CollapseRule::Is(Tile::Forest),
+            // CollapseRule::Is(Tile::DeepWater),
+            // CollapseRule::Is(Tile::Sand),
+        ]))),
+        CollapseRule::Not(Box::new(CollapseRule::Near(Box::new(CollapseRule::Is(Tile::Outer)), 8))),
+    ]);
 
     let tile_to_rule = |&tile| match tile {
         Tile::Unset => &unset_tile_rule,
         Tile::Outer => &outer_tile_rule,
+        Tile::DeepWater => &deep_water_tile_rule,
         Tile::Water => &water_tile_rule,
+        Tile::ShallowWater => &shallow_water_tile_rule,
         Tile::Sand => &sand_tile_rule,
         Tile::Grass => &grass_tile_rule,
         Tile::Forest => &forest_tile_rule,
@@ -104,10 +159,11 @@ fn main() {
         &history_grid,
         &tile_options[..],
         tile_to_rule,
-        1,
+        radius as isize,
         unset,
         outer,
         256,
+        seeds,
     );
 
     if let Some(generated_grid) = result {
@@ -125,6 +181,8 @@ fn main() {
                 Tile::Grass => Rgb([0, 255, 0]),
                 Tile::Forest => Rgb([0, 128, 0]),
                 Tile::Mountain => Rgb([128, 128, 128]),
+                Tile::DeepWater => Rgb([0, 0, 128]),
+                Tile::ShallowWater => Rgb([0, 128, 255]),
             };
 
             unsafe {
--- src/ffc/collapse_rules.rs
+++ src/ffc/collapse_rules.rs
@@ -2,6 +2,7 @@ use super::grid::Grid;
 use super::pos::Pos;
 use priority_queue::PriorityQueue;
 use rand::Rng;
+use std::cmp::Reverse;
 use std::hash::Hash;
 
 pub enum CollapseRule<T: PartialEq + Eq + Hash + Clone + Sync + Send> {
@@ -65,11 +66,11 @@ pub fn check_rule<T: PartialEq + Eq + Hash + Clone + Sync + Send>(
         CollapseRule::Not(sub_rule) => !sub_check_rule!(pos, sub_rule),
         CollapseRule::Is(tile_type) => {
             let tile = grid.get(pos, outer.clone());
-            tile == *tile_type || tile == unset
+            tile == unset || tile == *tile_type
         }
         CollapseRule::Was(tile_type) => {
-            let tile = history_grid.get(pos, outer.clone());
-            tile == *tile_type || tile == unset
+            let tile: T = history_grid.get(pos, outer.clone());
+            tile == unset || tile == *tile_type
         }
         CollapseRule::Left(sub_rule) => sub_check_rule!(&Pos { x: pos.x - 1, y: pos.y }, sub_rule),
         CollapseRule::Right(sub_rule) => sub_check_rule!(&Pos { x: pos.x + 1, y: pos.y }, sub_rule),
@@ -150,66 +151,103 @@ pub fn collapse_rule<'a, T: PartialEq + Eq + Hash + Clone + Sync + Send + 'a>(
     unset: T,
     outer: T,
     max_depth: usize,
+    seeds: usize,
 ) -> Option<Grid<T>> {
-    let mut placed_stack = vec![];
+    let mut placed_stack: Vec<(Pos, T, usize)> = vec![];
     let mut front = PriorityQueue::new();
 
     let mut rng = rand::rngs::ThreadRng::default();
 
-    let starting_point = rng.gen_range(0..grid.get_area());
-    front.push(starting_point, 0);
+    for _ in 0..seeds {
+        let starting_point = rng.gen_range(0..grid.get_area());
+        front.push(starting_point, Reverse(0));
+    }
 
-    let steps_to_print = grid.get_area() / 120;
+    let steps_to_print = grid.get_area() / 105;
     let mut clock_to_print = steps_to_print;
 
-    while !front.is_empty() {
+    macro_rules! find_valid_options {
+        ($find_pos: expr) => {
+            tile_options
+                .iter()
+                .filter(|tile_option| {
+                    check_rule(
+                        &grid,
+                        history_grid,
+                        $find_pos,
+                        tile_to_rule(tile_option),
+                        unset.clone(),
+                        outer.clone(),
+                        max_depth,
+                    )
+                })
+                .collect::<Vec<_>>()
+        };
+    }
+
+    while !front.is_empty()
+    /* && placed_stack.len() < grid.get_area() */
+    {
         if clock_to_print <= 0 {
             print!(
-                "Progress: {:3$}%\r",
+                "Progress: {: >3}%\r",
                 ((100.0 / grid.get_area() as f32) * placed_stack.len() as f32) as u32
             );
             clock_to_print = steps_to_print;
         }
         clock_to_print -= 1;
 
-        let (i, current_priority) = front.pop().expect("Front is empty but we just checked it");
+        let (i, Reverse(_current_priority)) = front.pop().expect("Front is empty but we just checked it");
+
         let pos = grid.i_to_pos(i);
 
-        if grid.get(&pos, outer.clone()) != unset {
+        let current_tile = grid.get(&pos, outer.clone());
+        if current_tile != unset || current_tile == outer {
             continue;
         }
 
-        let valid_options = tile_options
-            .iter()
-            .filter(|tile_option| {
-                check_rule(
-                    &grid,
-                    history_grid,
-                    &pos,
-                    tile_to_rule(tile_option),
-                    unset.clone(),
-                    outer.clone(),
-                    max_depth,
-                )
-            })
-            .collect::<Vec<_>>();
+        let valid_options = find_valid_options!(&pos);
+
+        // We've re-calculated this tile's valid options, and only want to evaluate the lowest-"entropy" option
+        if !front.is_empty() && valid_options.len() > front.peek().unwrap().1 .0 {
+            front.push(i, Reverse(valid_options.len()));
+            continue;
+        }
 
         if valid_options.is_empty() {
-            if placed_stack.is_empty() {
-                return None; // We failed to generated anything
+            front.push(i, Reverse(0));
+
+            let mut first_backtrack = true;
+
+            loop {
+                if placed_stack.is_empty() {
+                    return None; // We failed to generated anything
+                }
+
+                let (last_placed_pos, _, last_placed_attempts_remaining) =
+                    placed_stack.last().expect("Stack is empty but we just checked it");
+
+                if first_backtrack || *last_placed_attempts_remaining <= 0 {
+                    first_backtrack = false;
+                    grid.set(&last_placed_pos, unset.clone());
+
+                    // let last_placed_options: Vec<&T> = find_valid_options!(&last_placed_pos);
+                    // front.push(grid.pos_to_i(&last_placed_pos), last_placed_options.len());
+                    front.push(grid.pos_to_i(&last_placed_pos), Reverse(0));
+
+                    placed_stack.pop();
+                } else {
+                    placed_stack.last_mut().unwrap().2 = last_placed_attempts_remaining - 1;
+                    break;
+                }
             }
-            let (last_placed_pos, _, last_placed_attempts_remaining) =
-                placed_stack.pop().expect("Stack is empty but we just checked it");
-            grid.set(&last_placed_pos, unset.clone());
-            front.push(grid.pos_to_i(&last_placed_pos), current_priority + 1); // This ideally may put this tile off for later
-            front.push(i, current_priority + 1);
             continue;
         }
 
         let chosen_option = valid_options[rng.gen_range(0..valid_options.len())];
 
         grid.set(&pos, chosen_option.clone());
-        placed_stack.push((pos.clone(), chosen_option.clone(), 1));
+        placed_stack.push((pos.clone(), chosen_option.clone(), 15));
 
         // Now, for each unset neighbour within the re-check radius, we need to recheck it
         for dx in (-re_check_radius)..=re_check_radius {
@@ -218,26 +256,17 @@ pub fn collapse_rule<'a, T: PartialEq + Eq + Hash + Clone + Sync + Send + 'a>(
                     continue;
                 }
                 let neighbour_pos = pos.rel(dx, dy);
-                if grid.get(&neighbour_pos, outer.clone()) != unset {
+                if !grid.is_valid(&neighbour_pos) {
+                    continue;
+                }
+                let neighbour_tile = grid.get(&neighbour_pos, outer.clone());
+                if neighbour_tile != unset || neighbour_tile == outer {
                     continue;
                 }
                 // TODO: We should probably cache this somewhere and re-use it if no tiles within re_check_radius have changed (using a counter comparison?)
-                let neighbour_valid_options = tile_options
-                    .iter()
-                    .filter(|tile_option| {
-                        check_rule(
-                            &grid,
-                            history_grid,
-                            &neighbour_pos,
-                            tile_to_rule(tile_option),
-                            unset.clone(),
-                            outer.clone(),
-                            max_depth,
-                        )
-                    })
-                    .collect::<Vec<_>>();
-
-                front.push(grid.pos_to_i(&neighbour_pos), neighbour_valid_options.len());
+                let neighbour_valid_options = find_valid_options!(&neighbour_pos);
+                front.push(grid.pos_to_i(&neighbour_pos), Reverse(neighbour_valid_options.len()));
+                // front.push(grid.pos_to_i(&neighbour_pos), Reverse(0));
             }
         }
     }
